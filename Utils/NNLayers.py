58a9bb43d7e36cd82876865b47e2e51babd30408
Tendai Mukande <75079602+tmukande-debug@users.noreply.github.com>
2022-12-17 13:20:08 +0200

    Update NNLayers.py

--- Utils/NNLayers.py
+++ Utils/NNLayers.py
@@ -1,6 +1,4 @@
-#import tensorflow as tf
 import tensorflow as tf
-#from tf.keras.initializers.glorot_normal import xavier_initializer
 from tensorflow.contrib.layers import xavier_initializer
 import numpy as np
 
@@ -47,19 +45,19 @@ def defineParam(name, shape, dtype=tf.float32, reg=False, initializer='xavier', 
 	global regParams
 	assert name not in params, 'name %s already exists' % name
 	if initializer == 'xavier':
-		ret = tf.compat.v1.get_variable(name=name, dtype=dtype, shape=shape,
-			initializer=tf.keras.initializers.Initializer(),
+		ret = tf.get_variable(name=name, dtype=dtype, shape=shape,
+			initializer=xavier_initializer(dtype=tf.float32),
 			trainable=trainable)
 	elif initializer == 'trunc_normal':
 		ret = tf.get_variable(name=name, initializer=tf.random.truncated_normal(shape=[int(shape[0]), shape[1]], mean=0.0, stddev=0.03, dtype=dtype))
 	elif initializer == 'zeros':
-		ret = tf.compat.v1.get_variable(name=name, dtype=dtype,
+		ret = tf.get_variable(name=name, dtype=dtype,
 			initializer=tf.zeros(shape=shape, dtype=tf.float32),
 			trainable=trainable)
 	elif initializer == 'ones':
-		ret = tf.compat.v1.get_variable(name=name, dtype=dtype, initializer=tf.ones(shape=shape, dtype=tf.float32), trainable=trainable)
+		ret = tf.get_variable(name=name, dtype=dtype, initializer=tf.ones(shape=shape, dtype=tf.float32), trainable=trainable)
 	elif not isinstance(initializer, str):
-		ret = tf.compat.v1.get_variable(name=name, dtype=dtype,
+		ret = tf.get_variable(name=name, dtype=dtype,
 			initializer=initializer, trainable=trainable)
 	else:
 		print('ERROR: Unrecognized initializer')
@@ -123,27 +121,26 @@ def Bias(data, name=None, reg=False, reuse=False):
 		regParams[temBiasName] = bias
 	return data + bias
 
-##relu to Gelu
 def ActivateHelp(data, method):
 	if method == 'relu':
 		ret = tf.nn.relu(data)
 	elif method == 'sigmoid':
 		ret = tf.nn.sigmoid(data)
-	elif method == 'gelu':
-		ret = tf.keras.activations.gelu (data, approximate=False)
+	elif method == 'tanh':
+		ret = tf.nn.tanh(data)
 	elif method == 'softmax':
 		ret = tf.nn.softmax(data, axis=-1)
 	elif method == 'leakyRelu':
-		ret = tf.math.maximum(leaky*data, data)
+		ret = tf.maximum(leaky*data, data)
 	elif method == 'twoWayLeakyRelu6':
-		temMask = tf.compat.v1.to_float(tf.greater(data, 6.0))
-		ret = temMask * (6 + leaky * (data - 6)) + (1 - temMask) * tf.math.maximum(leaky * data, data)
+		temMask = tf.to_float(tf.greater(data, 6.0))
+		ret = temMask * (6 + leaky * (data - 6)) + (1 - temMask) * tf.maximum(leaky * data, data)
 	elif method == '-1relu':
-		ret = tf.math.maximum(-1.0, data)
+		ret = tf.maximum(-1.0, data)
 	elif method == 'relu6':
-		ret = tf.math.maximum(0.0, tf.minimum(6.0, data))
+		ret = tf.maximum(0.0, tf.minimum(6.0, data))
 	elif method == 'relu3':
-		ret = tf.math.maximum(0.0, tf.minimum(3.0, data))
+		ret = tf.maximum(0.0, tf.minimum(3.0, data))
 	else:
 		raise Exception('Error Activation Function')
 	return ret
@@ -207,7 +204,6 @@ def lightSelfAttention(localReps, number, inpDim, numHeads):
 	k = tf.reshape(tem, [-1, 1, number, numHeads, inpDim//numHeads])
 	v = tf.reshape(rspReps, [-1, 1, number, numHeads, inpDim//numHeads])
 	att = tf.nn.softmax(tf.reduce_sum(q * k, axis=-1, keepdims=True) / tf.sqrt(inpDim/numHeads), axis=2)
-	#att = tf.nn.softmax(tf.reduce_sum(q * k, axis=-1, keepdims=True) / (inpDim/numHeads), axis=2)
 	attval = tf.reshape(tf.reduce_sum(att * v, axis=2), [-1, number, inpDim])
 	rets = [None] * number
 	paramId = 'dfltP%d' % getParamId()
@@ -215,25 +211,3 @@ def lightSelfAttention(localReps, number, inpDim, numHeads):
 		tem1 = tf.reshape(tf.slice(attval, [0, i, 0], [-1, 1, -1]), [-1, inpDim])
 		rets[i] = tem1 + localReps[i]
 	return rets
-
-##tf reduce sum to einsum
-##Nystromformer
-##CosNormer
-##Transevolve
-
-def lightSelfAttention1(localReps, number, inpDim, numHeads):
-	Q = defineRandomNameParam([inpDim, inpDim], reg=False)
-	rspReps = tf.reshape(tf.stack(localReps, axis=1), [-1, inpDim])
-	tem = rspReps @ Q
-	q = tf.reshape(tem, [-1, number, 1, numHeads, inpDim//numHeads])
-	k = tf.reshape(tem, [-1, 1, number, numHeads, inpDim//numHeads])
-	v = tf.reshape(rspReps, [-1, 1, number, numHeads, inpDim//numHeads])
-	att = tf.nn.softmax(tf.reduce_sum(q * k, axis=-1, keepdims=True) / tf.sqrt(inpDim/numHeads), axis=2)
-	attval = tf.reshape(tf.reduce_sum(att * v, axis=2), [-1, number, inpDim])
-	rets = [None] * number
-	paramId = 'dfltP%d' % getParamId()
-	for i in range(number):
-		tem1 = tf.reshape(tf.slice(attval, [0, i, 0], [-1, 1, -1]), [-1, inpDim])
-		rets[i] = tem1 + localReps[i]
-	return rets
-
